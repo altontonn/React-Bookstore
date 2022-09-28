e75cb01fe0ee2830952c06a984a1a38e5f7b94a9
Newton <altontonnalumasa@gmail.com>
2022-09-28 17:27:23 +0300

    implement the functionality for checking status in categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkAction } from '../redux/categories/categories';
 
-class Category extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <button className="buttonCheck" type="submit">
-          Check status
-        </button>
-      </div>
-    );
-  }
+function Category() {
+  const status = useSelector((state) => state.checks);
+  const dispatch = useDispatch();
+  const handleCheck = () => {
+    dispatch(checkAction());
+  };
+  return (
+    <div>
+      <button className="buttonCheck" onClick={handleCheck} type="button">Check Status</button>
+      <h1>{status}</h1>
+    </div>
+  );
 }
 export default Category;
